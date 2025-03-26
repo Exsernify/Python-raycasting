55a5ac28d91132a906363378d99d927feec85965
Exsernify <134237509+Exsernify@users.noreply.github.com>
2025-03-26 09:50:32 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+import pygame
+from settings import *
+from map import *
+from player import *
+from raycaster import *
+
+screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+
+map = Map()
+player = Player()
+raycaster = Raycaster(player, map)
+
+background_image = pygame.image.load("background.png")
+
+clock = pygame.time.Clock()
+
+while True:
+    clock.tick(120)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    
+    player.update()
+    raycaster.castAllRays()
+
+    screen.blit(background_image, (0, 0, 10, 10))
+
+    # map.render(screen)
+    # player.render(screen)
+    raycaster.render(screen)
+    
+    pygame.display.update()
